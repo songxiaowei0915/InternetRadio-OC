918750f2332ffda4dcf37855bd22da26887a6e32
songxiaowei <songxiaowei0915@outlook.com>
2023-03-08 04:17:39 +0800

    empty project

---
+++ InternetRadio/InternetRadio/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  InternetRadio
+//
+//  Created by 宋小伟 on 2023/3/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ InternetRadio/InternetRadio/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  InternetRadio
+//
+//  Created by 宋小伟 on 2023/3/8.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
